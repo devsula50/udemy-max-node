d60f2ce499ff2c26ca3d56e3ca011cd8e7b8fc4d
devsula50 <devsula50@gmail.com>
2023-09-10 03:23:06 +0900

    bodyParser and post
    parsing something in the body and using app.post

--- section04/app.js
+++ section04/app.js
@@ -1,11 +1,21 @@
+const bodyParser = require('body-parser');
 const express = require('express')
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In another middleware!')
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title><button type="submit">Add Product</button></form>')
+})
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body)
+  res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
   res.send('<h1>Hello from Express!</h1>')
 })
-const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
