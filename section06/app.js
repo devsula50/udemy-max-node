8d21edf0a668df0b78edc88238a7dc1df86d9577
devsula50 <devsula50@gmail.com>
2023-09-12 17:36:04 +0900

    EJS와 부분적인 레이아웃
    EJS에서는 따로 제공하는 레이아웃 기능이 없다.
    단지 기본적으로 제공되는 include 기능을 사용하여 레이아웃 비슷한 기능을
    수행할 수 있다. 해당기능은 pug나 handlebars에서도 동일하게 제공된다.
    
    EJS에서 <% %> 태그 안에서 특별한 동작을 수행한다 마치 JSP와 비슷한
    것으로 보인다. 단순 expression 은 <%= %> 로 수행할 수 있다. statement 는
    <% %> 안에 if, for 문 등 필요한 것을 넣으면된다. 주의할 점은 <% %>
    안에서 { 를 열었다면 수행한 후에 뒤에서 닫아주는 <% %> 가 또한
    필요하다.
    
    다시 EJS 레이아웃으로 돌아오면 EJS 레이아웃은 비슷하게 <%- include('')
    %> 안에 미리 만들어둔 ejs파일 경로를 지정해주면된다. 그러면 사용하는
    곳에서 경로를 따라가서 존재하는 그 파일의 내용을 그대로 복사해서
    실행 시 보여준다.
    
    앞으로 EJS를 주로 사용할 것이기 때문에 나머지 html, pug, handlebars
    파일들은 삭제한다.

--- section06/app.js
+++ section06/app.js
@@ -2,16 +2,13 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.engine("hbs", expressHbs());
-// app.set("view engine", "pug");
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
